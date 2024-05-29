161115e473b76aa74d66aedf58a40f35dd6750c6
Gunjae Yoon <gjyoon@plaif.com>
2024-05-29 14:27:25 +0900

    Update main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <ipc-benchmark/version.h>
+#include <ipc-benchmark/base/performance.h>
+#include <ipc-benchmark/boost/shm_branch.h>
+#include <ipc-benchmark/boost/shm_trunk.h>
+#include <boost/core/demangle.hpp>
 #include <iostream>
+#include <typeinfo>
 
 using namespace ipc_benchmark;
 
 int main(int argc, char** argv) {
-	std::cout << "ipc-benchmark version: " << Version::full << std::endl;
+	logger.info("ipc-benchmark version:", Version::full);
+	const uint64_t count = 100;
+	const Performance* tests[] = { new ShmBranch(), new ShmTrunk() };
+
+	for (const Performance* test : tests) {
+		logger.info("test result of", boost::core::demangle(typeid(*test).name()), "is", test->run(count).count(), "nanoseconds");
+	}
 	return 0;
 }
