f01948c881def3f05a207c5d29a3a6c85d1bab43
Gunjae Yoon <gjyoon@plaif.com>
2024-05-29 10:18:38 +0900

    Add class interface for trunk shared memory tests

---
+++ src/ipc-benchmark/boost/shm_trunk.h
@@ -0,0 +1,16 @@
+#ifndef __ipc_benchmark_boost_shm_trunk_h__
+#define __ipc_benchmark_boost_shm_trunk_h__
+
+#include <ipc-benchmark/base/performance.h>
+
+namespace ipc_benchmark {
+	class ShmTrunk : Performance{
+	public:
+		ShmTrunk();
+		~ShmTrunk();
+
+		std::chrono::nanoseconds run(uint64_t count) const override;
+	};
+}
+
+#endif
