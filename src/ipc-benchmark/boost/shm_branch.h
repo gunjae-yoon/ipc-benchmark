3ad018d0b1a4ecf0d6af8bdef0a944114008f26d
Gunjae Yoon <gjyoon@plaif.com>
2024-05-29 10:20:20 +0900

    Add class interface for branch shared memory tests

---
+++ src/ipc-benchmark/boost/shm_branch.h
@@ -0,0 +1,16 @@
+#ifndef __ipc_benchmark_boost_shm_branch_h__
+#define __ipc_benchmark_boost_shm_branch_h__
+
+#include <ipc-benchmark/base/performance.h>
+
+namespace ipc_benchmark {
+	class ShmBranch : Performance{
+	public:
+		ShmBranch();
+		~ShmBranch();
+
+		std::chrono::nanoseconds run(uint64_t count) const override;
+	};
+}
+
+#endif
