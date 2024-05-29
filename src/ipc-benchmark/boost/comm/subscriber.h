a04a48d53346072351a4a946c67ec8f58545b9d9
Gunjae Yoon <gjyoon@plaif.com>
2024-05-29 15:21:51 +0900

    Add communication interface classes

---
+++ src/ipc-benchmark/boost/comm/subscriber.h
@@ -0,0 +1,19 @@
+#ifndef __ipc_benchmark_boost_comm_subscirber_h__
+#define __ipc_benchmark_boost_comm_subscirber_h__
+
+#include <ipc-benchmark/boost/base/types.h>
+#include <cstdint>
+#include <string>
+
+namespace ipc_benchmark {
+	class Subscriber {
+	public:
+		Subscriber(std::string shmname, std::string topicname);
+		~Subscriber();
+		
+		bool ready();
+		void run();
+	};
+}
+
+#endif
