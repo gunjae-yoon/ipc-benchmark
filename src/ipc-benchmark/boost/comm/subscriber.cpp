4fc5f991fee91e7fb87ebdb1488293088c528be9
Gunjae Yoon <gjyoon@plaif.com>
2024-05-30 09:20:10 +0900

    Implement ready sequences of Subscriber

--- src/ipc-benchmark/boost/comm/subscriber.cpp
+++ src/ipc-benchmark/boost/comm/subscriber.cpp
@@ -1,13 +1,37 @@
 #include <ipc-benchmark/boost/comm/subscriber.h>
+#include <thread>
 
 namespace ipc_benchmark {
 	Subscriber::Subscriber(std::string shmname, std::string topicname, std::uint64_t idx) {
+		name.shm = shmname;
+		name.topic = topicname;
+		id = idx;
 	}
 
 	Subscriber::~Subscriber() {
 	}
 
 	bool Subscriber::ready() {
+		segment = boost::interprocess::managed_shared_memory(boost::interprocess::open_only, name.shm.c_str());
+		
+		// TODO: need to make flexibly
+		mutex = segment.find<boost::interprocess::interprocess_mutex>("TrunkMutex").first;
+		if (mutex == nullptr) {
+			return false;
+		}
+
+		SharedMap* shmmap = segment.find<SharedMap>(name.topic.c_str()).first;
+		if (shmmap == nullptr) {
+			return false;;
+		}
+
+		const std::string shmcond = "TrunkCondition" + std::to_string(id);
+		cond = segment.find<boost::interprocess::interprocess_condition>(shmcond.c_str()).first;
+		vector = &shmmap->find(id)->second;
+		if ((cond == nullptr) || (vector == nullptr)) {
+			return false;
+		}
+
 		return true;
 	}
 
