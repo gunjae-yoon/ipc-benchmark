a945b91e49748475d61863bfec984e22006899fc
Gunjae Yoon <gjyoon@plaif.com>
2024-05-31 10:15:56 +0900

    Implement communication objects of POSIX message queue

--- src/ipc-benchmark/posix/mqueue/publisher.cpp
+++ src/ipc-benchmark/posix/mqueue/publisher.cpp
@@ -1,19 +1,51 @@
 #include <ipc-benchmark/posix/mqueue/publisher.h>
+#include <ipc-benchmark/base/performance.h>
 
 namespace ipc_benchmark::mqueue {
 	Publisher::Publisher(std::list<Proxy>& targets) {
 		for (Proxy& target : targets) {
-			proxies.push_back({target.topic, {}});
+			proxies.push_back({target.topic, -1});
 		}
 	}
 
 	Publisher::~Publisher() {
+		for (Proxy& proxy : proxies) {
+			if (proxy.desc != -1) {
+				mq_close(proxy.desc);
+			}
+		}
 	}
 
 	bool Publisher::ready() {
+		for (Proxy& proxy : proxies) {
+			proxy.desc = mq_open(proxy.topic.c_str(), O_RDWR);
+			if (proxy.desc == -1) {
+				return false;
+			}
+		}
+
 		return true;
 	}
 
 	void Publisher::run() {
+		char* buffer = new char[MqString::BUF_LEN];
+		if (buffer == nullptr) {
+			return;
+		}
+		
+		MqString data;
+		uint64_t published = 0;
+		while (published < Performance::DATA_AMOUNT) {
+			data.value = "value" + std::to_string(published);
+			data.serialize(buffer, MqString::BUF_LEN);
+			for (Proxy& proxy: proxies) {
+				if (mq_send(proxy.desc, buffer, MqString::BUF_LEN, 0) == -1) {
+					// failed to send message
+				}
+			}
+			published++;
+		}
+
+		delete[](buffer);
 	}
 }
