112b1afe6fed1b201474361ed580b7b902518540
Gunjae Yoon <gjyoon@plaif.com>
2024-05-31 10:15:56 +0900

    Implement communication objects of POSIX message queue

--- src/ipc-benchmark/posix/mqueue/subscriber.cpp
+++ src/ipc-benchmark/posix/mqueue/subscriber.cpp
@@ -1,13 +1,44 @@
 #include <ipc-benchmark/posix/mqueue/subscriber.h>
+#include <ipc-benchmark/base/performance.h>
 
 namespace ipc_benchmark::mqueue {
-	Subscriber::Subscriber(std::string topicname) : topic(topicname), desc(), thread(nullptr), buffer({MqString::BUF_LEN, new char[MqString::BUF_LEN]}) {
+	Subscriber::Subscriber(std::string topicname) : topic(topicname), desc(-1), thread(nullptr), buffer({MqString::BUF_LEN, new char[MqString::BUF_LEN]}) {
 	}
 
 	Subscriber::~Subscriber() {
+		if ((thread != nullptr) && (thread->joinable())) {
+			thread->join();
+		}
+		delete(thread);
+		thread = nullptr;
+
+		if (buffer.mem != nullptr) {
+			delete[](buffer.mem);
+		}
+		
+		if (desc != -1) {
+			mq_close(desc);
+		}
 	}
 
 	bool Subscriber::ready() {
+		desc = mq_open(topic.c_str(), O_RDWR);
+		if (desc == -1) {
+			return false;
+		}
+
+		thread = new std::thread([&](){
+			uint64_t received = 0;
+			MqString data;
+			while (received < Performance::DATA_AMOUNT) {
+				if (mq_receive(desc, buffer.mem, MqString::BUF_LEN, 0) == -1) {
+					break;
+				}
+				data.deserialize(buffer.mem);
+				received++;
+			}
+		});
+
 		return true;
 	}
 
@@ -15,5 +46,8 @@ namespace ipc_benchmark::mqueue {
 	}
 
 	void Subscriber::wait() {
+		if ((thread != nullptr) && (thread->joinable())) {
+			thread->join();
+		}
 	}
 }
