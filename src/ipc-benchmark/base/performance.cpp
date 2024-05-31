6d569ef4098635416606c982c5ec51e71e83218f
Gunjae Yoon <gjyoon@plaif.com>
2024-05-31 10:28:31 +0900

    Update log target to ALL from CONSOLE

--- src/ipc-benchmark/base/performance.cpp
+++ src/ipc-benchmark/base/performance.cpp
@@ -1,7 +1,7 @@
 #include <ipc-benchmark/base/performance.h>
 
 namespace ipc_benchmark {
-	guutil::log::Logger logger("global", guutil::log::Level::INFO);
+	guutil::log::Logger logger("global", guutil::log::Level::INFO, guutil::log::Target::ALL);
 	
 	const uint64_t Performance::DATA_AMOUNT = 10000;
 }
