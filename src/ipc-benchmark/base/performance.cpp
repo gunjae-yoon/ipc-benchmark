78e34d0bd8bfe785e15bd55d8c3c91dadf10be4d
Gunjae Yoon <gjyoon@plaif.com>
2024-05-31 10:19:37 +0900

    Change log level to INFO from ALL

--- src/ipc-benchmark/base/performance.cpp
+++ src/ipc-benchmark/base/performance.cpp
@@ -1,7 +1,7 @@
 #include <ipc-benchmark/base/performance.h>
 
 namespace ipc_benchmark {
-	guutil::log::Logger logger("global");
+	guutil::log::Logger logger("global", guutil::log::Level::INFO);
 	
 	const uint64_t Performance::DATA_AMOUNT = 10000;
 }
